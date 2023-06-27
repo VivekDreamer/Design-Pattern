c7978a737fea62ae0dafa65426577a1391afb2c8
VivekDreamer <vivekmakv@gmail.com>
2023-06-27 22:43:31 +0530

    Code without strategy pattern : 1st commit

---
+++ src/main/java/com/vivek/WithoutStrategicDesignPattern/Vehicle.java
@@ -0,0 +1,10 @@
+package com.vivek.WithoutStrategicDesignPattern;
+
+//base class
+//code duplication is the problem here
+public class Vehicle {
+    public void drive(){
+        //some code
+        System.out.println("normal drive capability"); 
+    }
+}
