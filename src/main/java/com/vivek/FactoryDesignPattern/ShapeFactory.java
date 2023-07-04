89a65d6b3996e244bf4e112c895fe3f36ad37554
VivekDreamer <vivekmakv@gmail.com>
2023-07-04 23:14:10 +0530

    factory class added

---
+++ src/main/java/com/vivek/FactoryDesignPattern/ShapeFactory.java
@@ -0,0 +1,11 @@
+package com.vivek.FactoryDesignPattern;
+
+public class ShapeFactory {
+    Shape getShape(String input){
+        switch(input){
+            case "circle" : return new Circle();
+            case "rectangle" : return new Rectangle();
+            default : return null;
+        }
+    }
+}
