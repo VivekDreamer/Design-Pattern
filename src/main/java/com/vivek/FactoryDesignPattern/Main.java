d8a74fb6127f7868e7fbed241d537ae8d62462f8
VivekDreamer <vivekmakv@gmail.com>
2023-07-04 23:14:28 +0530

    client code added

---
+++ src/main/java/com/vivek/FactoryDesignPattern/Main.java
@@ -0,0 +1,9 @@
+package com.vivek.FactoryDesignPattern;
+
+public class Main {
+    public static void main(String[] args) {
+        ShapeFactory shapeFactory = new ShapeFactory();
+        Shape shape = shapeFactory.getShape("circle");
+        shape.draw(); 
+    }
+}
