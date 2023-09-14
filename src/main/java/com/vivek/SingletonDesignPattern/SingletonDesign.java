93213f20f69c51f7f7a92e303f3730c65ea3bfe3
VivekDreamer <vivekmakv@gmail.com>
2023-09-14 17:08:36 +0000

    Singleton design pattern is created by using eager initialization.

---
+++ src/main/java/com/vivek/SingletonDesignPattern/SingletonDesign.java
@@ -0,0 +1,22 @@
+package com.vivek.SingletonDesignPattern;
+
+public class SingletonDesign {
+    public static void main(String[] args) {
+        A obj1 = A.getInstance();
+        A obj2 = A.getInstance();
+        System.out.println(obj1.hashCode());
+        System.out.println(obj2.hashCode());
+    }
+}
+//Eager initialization
+//static object will get created at the time class loads
+//Even if you are not using this object, it will be there in the memory
+//If this object consumes lots of resources, then, it will be the waste of memory
+//this type of object creation in singleton is called eager initialization.
+class A{
+    static A obj = new A();
+    private A(){}
+    public static A getInstance(){
+        return obj;
+    }
+}
