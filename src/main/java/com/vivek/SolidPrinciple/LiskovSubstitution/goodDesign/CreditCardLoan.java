df236d3f4609b246895af58e73972877b7b73a44
VivekDreamer <vivekmakv@gmail.com>
2023-09-27 17:58:17 +0000

    child class of LoanPayment interface i.e, CreditCardLoan {Good Design}

---
+++ src/main/java/com/vivek/SolidPrinciple/LiskovSubstitution/goodDesign/CreditCardLoan.java
@@ -0,0 +1,10 @@
+package com.vivek.SolidPrinciple.LiskovSubstitution.goodDesign;
+
+public class CreditCardLoan implements LoanPayment{
+
+    @Override
+    public void doPayment(int amount) {
+        
+    }
+    
+}
