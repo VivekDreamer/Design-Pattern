079016587b13014c4495afa44906d7e494a621bd
VivekDreamer <vivekmakv@gmail.com>
2023-10-11 11:50:01 +0000

    Bridge Pattern : YoutubeVideo class is added.

---
+++ src/main/java/com/vivek/BridgePattern/YoutubeVideo.java
@@ -0,0 +1,13 @@
+package com.vivek.BridgePattern;
+
+public class YoutubeVideo extends Video{
+
+    public YoutubeVideo(VideoProcessor processor) {
+        super(processor);
+    }
+
+    @Override
+    public void play(String videoFile) {
+        
+    }    
+}
