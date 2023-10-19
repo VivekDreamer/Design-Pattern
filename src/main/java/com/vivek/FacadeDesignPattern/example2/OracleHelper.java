2e02b2b5908aad4d8b97bb35f68237d96435f198
VivekDreamer <vivekmakv@gmail.com>
2023-10-19 13:49:43 +0000

    Facade Design Pattern Example 2: OracleHelper class is added.

---
+++ src/main/java/com/vivek/FacadeDesignPattern/example2/OracleHelper.java
@@ -0,0 +1,17 @@
+package com.vivek.FacadeDesignPattern.example2;
+
+import java.sql.Connection;
+
+public class OracleHelper {
+    public static Connection getOracleDBConnection(){
+        //get Oracle DB connection using connection parameters
+        return null;
+    }
+    public void generateOraclePDFReport(String tableName, Connection con){
+		//get data from table and generate pdf report
+	}
+	
+	public void generateOracleHTMLReport(String tableName, Connection con){
+		//get data from table and generate pdf report
+	}
+}
