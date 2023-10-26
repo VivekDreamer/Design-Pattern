685a325c0f9eeb3a007f615f3261c7284719568e
VivekDreamer <vivekmakv@gmail.com>
2023-10-26 12:54:13 +0530

    second example of builder design pattern: Client code is modified.

--- src/main/java/com/vivek/BuilderDesignPattern/second_example/Main.java
+++ src/main/java/com/vivek/BuilderDesignPattern/second_example/Main.java
@@ -12,6 +12,19 @@ public class Main {
         System.out.println(url.getHostname());
         System.out.println(url.getPort());
         System.out.println(url.getPathParam());
-        
+
+        URLBuilder.Builder builder = new URLBuilder.Builder();
+        builder.protocol("https://").hostname("mywebsite:").port("443");
+        URLBuilder urlBuilder = builder.build();
+        System.out.print(urlBuilder.protocol);
+        System.out.print(urlBuilder.hostname);
+        System.out.println(urlBuilder.port);
+
+        URLBuilder.Builder builder2 = new URLBuilder.Builder();
+        builder2.protocol("https://").hostname("mywebsite2").pathParam("/abc");
+        URLBuilder urlBuilder2 = builder2.build();
+        System.out.print(urlBuilder2.protocol);
+        System.out.print(urlBuilder2.hostname);
+        System.out.print(urlBuilder2.pathParam );
     }
 }
