313ec15b0d127d995cbc352e0477b87b16acbd38
VivekDreamer <vivekmakv@gmail.com>
2023-10-27 19:10:31 +0530

    Chain of responsibility design pattern : BankPaymentHandler implementation class is added.

---
+++ src/main/java/com/vivek/ChainOfResponsibility/BankPaymentHandler.java
@@ -0,0 +1,14 @@
+package com.vivek.ChainOfResponsibility;
+
+public class BankPaymentHandler extends PaymentHandler{
+
+    @Override
+    public void handlePayment(double amount) {
+        if(amount <= 500){
+            System.out.println("Paid using bank account: $"+amount);
+        }else{
+            next.handlePayment(amount);
+        }
+    }
+    
+}
