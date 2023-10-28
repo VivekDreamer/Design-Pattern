752b0caa34e71b38b800c3aebfbb596ef0da5814
VivekDreamer <vivekmakv@gmail.com>
2023-10-28 18:48:11 +0530

    Chain of responsibility design pattern : CreditCardPaymentHandler implementation class is added.

---
+++ src/main/java/com/vivek/ChainOfResponsibility/CreditCardPaymentHandler.java
@@ -0,0 +1,14 @@
+package com.vivek.ChainOfResponsibility;
+
+public class CreditCardPaymentHandler extends PaymentHandler{
+
+    @Override
+    public void handlePayment(double amount) {
+        if(amount <= 1000){
+            System.out.println("Paid using credit card: $"+amount);
+        }else{
+            next.handlePayment(amount);
+        }
+    }
+    
+}
