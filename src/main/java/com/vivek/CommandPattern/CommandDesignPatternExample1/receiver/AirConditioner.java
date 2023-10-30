61b6025e6318b1f9cd5980f9824768674c9ebeef
VivekDreamer <vivekmakv@gmail.com>
2023-10-30 16:47:40 +0530

    Command Design pattern example 1 : AirConditioner class is added.

---
+++ src/main/java/com/vivek/CommandPattern/CommandDesignPatternExample1/receiver/AirConditioner.java
@@ -0,0 +1,18 @@
+package com.vivek.CommandPattern.CommandDesignPatternExample1.receiver;
+
+public class AirConditioner {
+    boolean isOn;
+    int temperature;
+    public void turnOnAC(){
+        isOn = true;
+        System.out.println("AC is ON");
+    }
+    public void turnOffAC(){
+        isOn = false;
+        System.out.println("AC is OFF");
+    }
+    public void setTemperature(int temp){
+        this.temperature = temp;
+        System.out.println("Temperature changes to : "+temp);
+    }
+}
