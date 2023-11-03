82d503f12fc515e20dc428945f98993ddfd235fa
VivekDreamer <vivekmakv@gmail.com>
2023-11-03 15:27:11 +0530

    Factory design pattern example3 :CourseFactory class is added.

---
+++ src/main/java/com/vivek/FactoryDesignPattern/example3/CourseFactory.java
@@ -0,0 +1,14 @@
+package com.vivek.FactoryDesignPattern.example3;
+
+public class CourseFactory {
+    public static Course getCourse(String courseType){
+        switch (courseType){
+            case "LLD":
+                return new LLD();
+            case "HLD":
+                return new HLD();
+            default:
+                return null;
+        }
+    }
+}
