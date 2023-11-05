20e2984d8f39f562c1b95994454501ea07da8d0a
VivekDreamer <vivekmakv@gmail.com>
2023-11-06 01:10:50 +0530

    2nd example of Abstract Factory Design Pattern : GeorgiaTechFeeCalculator child class is added.

---
+++ src/main/java/com/vivek/AbstractFactoryDesignPattern/example2/GeorgiaTechFeeCalculator.java
@@ -0,0 +1,10 @@
+package com.vivek.AbstractFactoryDesignPattern.example2;
+
+public class GeorgiaTechFeeCalculator extends FeeCalculator{
+
+    @Override
+    public int calculateFee() {
+        return 55000;
+    }
+
+}
