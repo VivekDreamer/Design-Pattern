877b9ae325174ed561d065ccd55957d0e17f0104
VivekDreamer <vivekmakv@gmail.com>
2023-11-06 01:10:12 +0530

    2nd example of Abstract Factory Design Pattern : CalTechFeeCalculator child class is added.

---
+++ src/main/java/com/vivek/AbstractFactoryDesignPattern/example2/CalTechFeeCalculator.java
@@ -0,0 +1,10 @@
+package com.vivek.AbstractFactoryDesignPattern.example2;
+
+public class CalTechFeeCalculator extends FeeCalculator{
+
+    @Override
+    public int calculateFee() {
+        return 40000;
+    }
+
+}
