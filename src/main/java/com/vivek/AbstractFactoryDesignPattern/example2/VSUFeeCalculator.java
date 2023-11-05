b0dc2885bb9502c2fa90860871c4555da7c90b26
VivekDreamer <vivekmakv@gmail.com>
2023-11-06 01:12:28 +0530

    2nd example of Abstract Factory Design Pattern : VSUFeeCalculator child class is added.

---
+++ src/main/java/com/vivek/AbstractFactoryDesignPattern/example2/VSUFeeCalculator.java
@@ -0,0 +1,10 @@
+package com.vivek.AbstractFactoryDesignPattern.example2;
+
+public class VSUFeeCalculator extends FeeCalculator{
+
+    @Override
+    public int calculateFee() {
+        return 66000;
+    }
+
+}
