4a7f6ba2df1eef3cc55be1208feb00044710395d
VivekDreamer <vivekmakv@gmail.com>
2023-11-06 01:12:01 +0530

    2nd example of Abstract Factory Design Pattern : USCCalculator child class is added.

---
+++ src/main/java/com/vivek/AbstractFactoryDesignPattern/example2/USCCalculator.java
@@ -0,0 +1,10 @@
+package com.vivek.AbstractFactoryDesignPattern.example2;
+
+public class USCCalculator extends FeeCalculator{
+
+    @Override
+    public int calculateFee() {
+        return 99000;
+    }
+
+}
