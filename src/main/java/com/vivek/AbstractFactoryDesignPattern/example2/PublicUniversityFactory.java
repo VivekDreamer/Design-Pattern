4611e01b7e166f18dd48d558d212ea2f969a1eaf
VivekDreamer <vivekmakv@gmail.com>
2023-11-07 08:54:42 +0530

    2nd example of Abstract Factory Design Pattern : PublicUniversityFactory child class is added.

---
+++ src/main/java/com/vivek/AbstractFactoryDesignPattern/example2/PublicUniversityFactory.java
@@ -0,0 +1,35 @@
+package com.vivek.AbstractFactoryDesignPattern.example2;
+
+public class PublicUniversityFactory extends UniversityFactory {
+
+    @Override
+    public AdmitCard getAdmitCard(String course) {
+        switch (course) {
+            case "Math":
+                return new USCAdmitCard();
+            case "Physics":
+                return new VSUAdmitCard();
+            case "CS":
+                return new ASUAdmitCard();
+            default:
+                break;
+        }
+        return null;
+    }
+
+    @Override
+    public FeeCalculator getFeeCalculator(String course) {
+        switch (course) {
+            case "Math":
+                return new USCCalculator();
+            case "Physics":
+                return new VSUFeeCalculator();
+            case "CS":
+                return new ASUFeeCalculator();
+            default:
+                break;
+        }
+        return null;
+    }
+
+}
