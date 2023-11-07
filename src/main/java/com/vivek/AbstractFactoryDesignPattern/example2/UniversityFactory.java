bc606f543a7c13b412ee70d104c2df9d187e0920
VivekDreamer <vivekmakv@gmail.com>
2023-11-07 08:53:51 +0530

    2nd example of Abstract Factory Design Pattern : UniversityFactory abstract class is added.

---
+++ src/main/java/com/vivek/AbstractFactoryDesignPattern/example2/UniversityFactory.java
@@ -0,0 +1,18 @@
+package com.vivek.AbstractFactoryDesignPattern.example2;
+
+public abstract class UniversityFactory {
+
+    public static int cutoff = 109;
+    public static UniversityFactory getUniversityFactory(int entranceExamScore) {
+        if(entranceExamScore > cutoff){
+            return new IvyLeagueUniversityFactory();
+        }else{
+            return new PublicUniversityFactory();
+        }
+    }
+
+    public abstract AdmitCard getAdmitCard(String string);
+
+    public abstract FeeCalculator getFeeCalculator(String string);
+
+}
