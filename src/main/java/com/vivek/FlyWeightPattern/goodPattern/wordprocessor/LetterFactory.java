34bdd5abc0c191ff56c32f75e05928e508ed093b
VivekDreamer <vivekmakv@gmail.com>
2023-11-13 18:56:12 +0530

    Flyweight pattern-> LetterFactory class added for word processor.

---
+++ src/main/java/com/vivek/FlyWeightPattern/goodPattern/wordprocessor/LetterFactory.java
@@ -0,0 +1,17 @@
+package com.vivek.FlyWeightPattern.goodPattern.wordprocessor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LetterFactory {
+    private static Map<Character, ILetter> characterCache = new HashMap<>();
+    public static ILetter createLetter(char characterValue){
+        if(characterCache.containsKey(characterValue)){
+            return characterCache.get(characterValue);
+        }else{
+            DocumentCharacter characterObj = new DocumentCharacter(characterValue, "Arial", 10);
+            characterCache.put(characterValue, characterObj);
+            return characterObj;
+        }
+    }
+}
